e491f75d9fe39a95399864e63488877729a59217
DIANNA CADEN <44316764+Me-in-U@users.noreply.github.com>
2024-12-24 05:37:49 +0900

    이미지 처리

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import discord
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
-from clientGPT import send_to_chatgpt
+from clientGPT import image_analysis, send_to_chatgpt
 from riot import get_rank_data
 
 # 환경 변수를 .env 파일에서 로딩
@@ -52,7 +52,7 @@ async def on_ready():
     await load_json()  # settingData.json 로드
     await load_all_nicknames()  # 채널의 모든 멤버 닉네임 저장
     await load_recent_messages()  # 최근 메시지 로드
-    await update_presence()
+    await update_presence()  # rich presence update
 
     reset_user_messages.start()  # 자정 루프 시작
     presence_update_task.start()  # 1분마다 Presence 업데이트 태스크 시작
@@ -88,8 +88,30 @@ async def question(ctx):
     ChatGPT
     """
     user_messages[ctx.author].append({"role": "user", "content": ctx.message.content})
+
     # ChatGPT에 메시지 전달
-    response = send_to_chatgpt(user_messages[ctx.author], temperature=0.4)
+    messages = [
+        {
+            "role": "system",
+            "content": """아래는 유저가 말했던 기록이다.
+            내용을 참고하도록 하고 맨 마지막이 질문이다.
+            질문에 대한 답을 해라. 전체 대화 내용이 필요한 질문이면 밑에서 참고해라
+            추가적인 질문요청, 궁금한점이 있는지 되묻지 말고 질문에만 답해라""",
+        },
+        {
+            "role": "system",
+            "content": f"전체 대화 내용 : {user_messages}",
+        },
+        {
+            "role": "user",
+            "content": f"{ctx.author}의 질문 : {ctx.message.content}",
+        },
+        {"role": "system", "content": f"아래는 닉네임 정보:\n{nicknames}\n"},
+    ]
+
+    # ChatGPT에 메시지 전달
+    response = send_to_chatgpt(messages, temperature=0.4)
+
     # 봇 응답 기록
     user_messages[ctx.author].append({"role": "assistant", "content": response})
     await ctx.reply(f"{response}")
@@ -99,29 +121,37 @@ async def question(ctx):
     aliases=["신이시여", "신이여", "창섭님"],
     help="정상화의 신에게 질문합니다. '!신이시여 [질문 내용]' 형식으로 사용하세요.",
 )
-async def to_god(ctx, *, text: str = None):
+async def to_god(ctx):
     """
     커맨드 질문 처리
     ChatGPT
     """
-    message = text.strip() if text else ""
+    user_messages[ctx.author].append({"role": "user", "content": ctx.message.content})
 
+    # ChatGPT에 메시지 전달
     messages = [
         {
             "role": "system",
-            "content": "당신은 세계 최고 정상화의 신, 신창섭 디렉터이다. 당신은 모든것을 정상화 하는 능력이 있다. 신으로써 아래 질문에 대한 답을 해야한다. 당신은 모든것을 알고있다. 이에 답을하라",
+            "content": """당신은 세계 최고 정상화의 신, 게임 메이플스토리의 신창섭 디렉터이다.
+            당신은 모든것을 정상화 하는 능력이 있다. 신으로써 아래 질문에 대한 답을 해야한다.
+            당신은 모든것을 알고있다. 이에 답을하라. 정상화의 신이 말하는 말투로 말해라.
+            문제가 있다면 해결하는 방향으로 정상화 시켜라.
+            아래는 유저가 말했던 기록이다. 내용을 참고하도록 하고 맨 마지막이 질문이다.""",
         },
         {
             "role": "system",
-            "content": "정상화의 신이 말하는 말투로 말해라. 문제가 있다면 해결하는 방향으로 정상화 시켜라",
+            "content": f"전체 대화 내용 : {user_messages}",
         },
         {
             "role": "user",
-            "content": message,
+            "content": f"{ctx.author}의 질문 : {ctx.message.content}",
         },
+        {"role": "system", "content": f"아래는 닉네임 정보:\n{nicknames}\n"},
     ]
+
     # ChatGPT에 메시지 전달
     response = send_to_chatgpt(messages, temperature=0.7)
+
     # 봇 응답 기록
     user_messages[ctx.author].append({"role": "assistant", "content": response})
     await ctx.reply(f"{response}")
@@ -147,17 +177,11 @@ async def summary(ctx, *, text: str = None):
     messages = [
         {
             "role": "system",
-            "content": "당신은 요약 전문가입니다. 주어진 대화 내용을 요약해주세요. ",
-        },
-        {
-            "role": "system",
-            "content": """전체적인 내용을 5줄 이내로 요약. 
-            그 이후 각 유저가 한 말을 따로 요약한걸 추가해줘 
-            닉네임 : 요약 형식으로 """,
-        },
-        {
-            "role": "system",
-            "content": "자연스러운 말투로 말하기",
+            "content": """당신은 요약 전문가입니다. 주어진 대화 내용을 요약해주세요.
+            전체적인 내용을 5줄 이내로 요약. 
+            그 이후 각 유저가 한 말을 따로 요약한걸 추가해줘.
+            닉네임 : 요약 형식으로
+            자연스러운 말투로 말해줘.""",
         },
         {"role": "system", "content": f"추가 요청 사항 : {request_message}"},
         {
@@ -226,14 +250,23 @@ async def interpret(ctx, *, text: str = None):
     """
     입력된 문장이 있으면 해당 문장을, 없으면 최근 메시지를 해석합니다.
     """
+    target_message = ""
+    image_url = None
+
     if text:
         # 명령어 뒤에 입력된 문장이 있을 경우 해당 문장 번역
         target_message = text.strip()
+        # 이미지 첨부 여부 확인
+        if ctx.message.attachments:
+            image_url = ctx.message.attachments[0].url
     else:
         # 최근 메시지 탐색
         async for message in ctx.channel.history(limit=10):  # 최근 최대 10개 탐색
             if message.author != client.user and message.id != ctx.message.id:
                 target_message = message.content
+                # 이미지 첨부 여부 확인
+                if message.attachments:
+                    image_url = message.attachments[0].url
                 break
         else:
             # 번역할 메시지가 없을 경우
@@ -252,11 +285,15 @@ async def interpret(ctx, *, text: str = None):
         },
     ]
 
-    # ChatGPT에 메시지 전달
-    translated_message = send_to_chatgpt(messages, temperature=0.6)
+    if image_url:
+        # 이미지와 텍스트를 처리
+        interpreted = image_analysis(messages, image_url=image_url, temperature=0.6)
+    else:
+        # 텍스트만 처리
+        interpreted = send_to_chatgpt(messages, temperature=0.6)
 
     # 번역 결과 출력
-    await ctx.reply(translated_message)
+    await ctx.reply(interpreted)
 
 
 @client.command(
