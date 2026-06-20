c365ed0090eedcc2e41e59cf95d1d8713ecff5bf
김민규(KIM MINGYU) <jun3021303@naver.com>
2026-06-20 21:43:32 +0900

    검색 프롬프트 버전 업데이트

---
+++ test/test_search_prompt.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+
+os.environ.setdefault("OPENAI_KEY", "test-key")
+
+from cogs.search import SEARCH_PROMPT_ID, SEARCH_PROMPT_VERSION
+
+
+class SearchPromptTests(unittest.TestCase):
+    def test_search_command_uses_prompt_version_2(self):
+        self.assertEqual(
+            SEARCH_PROMPT_ID,
+            "pmpt_68b25c89c1a48193a60de5a3cb23a1eb0c25a13613efd1bf",
+        )
+        self.assertEqual(SEARCH_PROMPT_VERSION, "2")
+
+
+if __name__ == "__main__":
+    unittest.main()
