72f0d04da54206021c63ad09e6930c0690602a19
김민규(KIM MINGYU) <jun3021303@naver.com>
2026-06-20 21:48:19 +0900

    검색 프롬프트 버전 3 업데이트

--- test/test_search_prompt.py
+++ test/test_search_prompt.py
@@ -7,12 +7,12 @@ from cogs.search import SEARCH_PROMPT_ID, SEARCH_PROMPT_VERSION
 
 
 class SearchPromptTests(unittest.TestCase):
-    def test_search_command_uses_prompt_version_2(self):
+    def test_search_command_uses_prompt_version_3(self):
         self.assertEqual(
             SEARCH_PROMPT_ID,
             "pmpt_68b25c89c1a48193a60de5a3cb23a1eb0c25a13613efd1bf",
         )
-        self.assertEqual(SEARCH_PROMPT_VERSION, "2")
+        self.assertEqual(SEARCH_PROMPT_VERSION, "3")
 
 
 if __name__ == "__main__":
